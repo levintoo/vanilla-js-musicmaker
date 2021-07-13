57954e4b14de4d772205cf73503c2441c5c89de8
levintoo <tooklevin@gmail.com>
2021-07-13 11:51:39 +0300

    index playy

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 window.addEventListener('load', ()=> {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+
+
+    
+    pads.forEach((pad, index) =>{
+        pad.addEventListener("click", function(){
+            sounds[index].play();
+
+        });
+    });
 });
+
