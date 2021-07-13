323582107ddc07f15f26813fb005a6d7c2095d51
levintoo <tooklevin@gmail.com>
2021-07-13 12:00:23 +0300

    #

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 window.addEventListener('load', ()=> {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#60b2d3"
+    ]
 
 
     
     pads.forEach((pad, index) =>{
         pad.addEventListener("click", function(){
+
+            sounds[index].currentTime = 0;
             sounds[index].play();
 
         });
     });
+
+    // make bubbles
 });
 
